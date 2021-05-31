2c4c2a63526c79b11889286d2c8eea1076625848
clementwahaga <c.wahaga@cci-formation.nc>
2021-06-01 08:30:00 +1100

    upadte README

--- header.php
+++ header.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Original+Surfer&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap" rel="stylesheet">
     <?php
           wp_enqueue_style('style.css');
     ?>
@@ -18,15 +22,19 @@
 
 <body <?php body_class(); ?>>
 
-<div class="header">
-    <header class="admin-bar">
-        <a href="<?php echo home_url( '/' ); ?>">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/logo.jpg" alt="Logo">
-        </a>
-        
-    </header>
-    <?php wp_nav_menu( array( 'theme_location' => 'mymenu' ) );?>
-</div>
+
         
     
-    <?php wp_body_open(); ?>
+    <?php wp_body_open(); ?>-->
+    <nav>
+            le pays
+    </nav>
+    <div class="container">
+        <div><i class="fas fa-running"></i></div>
+        <div><i class="far fa-newspaper"></i></div>
+        <div><i class="fas fa-cloud-sun"></i></div>
+        <div><i class="fas fa-handshake"></i></div>
+        
+    </div>
+    
+    
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <div class="contain">
     <div>
-        <h1>Xerxes</h1>
+        <h1></h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident fuga cumque quos minus eaque in, enim rem consectetur quidem adipisci cupiditate earum nostrum labore ipsam cum, alias non vel blandit</p>
     </div>
    
